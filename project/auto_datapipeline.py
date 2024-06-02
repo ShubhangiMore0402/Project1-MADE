d69ee84adaa981e54b5df799e5431aca644db16f
ShubhangiMore0402 <119363324+ShubhangiMore0402@users.noreply.github.com>
2024-06-02 17:27:14 +0200

    Update auto_datapipeline.py

--- project/auto_datapipeline.py
+++ project/auto_datapipeline.py
@@ -2,52 +2,26 @@ import pandas as pd
 from sqlalchemy import create_engine, TEXT, FLOAT, BIGINT
 import numpy as np
 import os
+import requests
+import io
+
 
 def drop_columns(df, col_list):
-    """
-    This will remove specified columns from a pandas DataFrame.
-
-    Parameters:
-    df (pd.DataFrame): This is the DataFrame from which specified columns are to be removed.
-    col_list (list): A list of strings shows the names of the columns which are to be removed.
-
-    Returns:
-    pd.DataFrame: It returns the modified DataFrame with the specified columns which are removed.
-    """
-
+    
+    existing_cols = [col for col in col_list if col in df.columns]
     # Dropping the specified columns from the DataFrame
-    df.drop(col_list, axis=1, inplace=True)
+    df.drop(existing_cols, axis=1, inplace=True)
 
     return df
 
 def rename_columns(df, col_dict):
-    """
-    Columns of a pandas DataFrame are renamed based on a provided dictionary mapping.
-
-    Parameters:
-    df (pd.DataFrame): These are the DataFrame whose columns are to be renamed.
-    col_dict (dict): A dictionary mapping the current column names to the new column names.
-
-    Returns:
-    pd.DataFrame: The DataFrame with columns renamed as it is specified in the col_dict.
-    """
-
+  
     # Renaming columns of the DataFrame as per the provided dictionary mapping
     df.rename(columns=col_dict, inplace=True)
 
     return df
 
 def fix_date_format(df, col_list):
-    """
-    Arranging the format of date columns the DataFrame.
-
-    Parameters:
-    df (pd.DataFrame): The DataFrame which contains the date columns which are to be formatted.
-    col_list (list): A list of column names (strings) in the DataFrame that contains date information.
-
-    Returns:
-    pd.DataFrame: The DataFrame with adjusted date formats in the specified columns.
-    """
 
     # Iterating over each column in the provided column list
     for col in col_list:
@@ -55,110 +29,90 @@ def fix_date_format(df, col_list):
         df[col] = df[col].dt.strftime('%Y-%m-%d %H:%M')   
     return df
 
+def transpose_data(df):
+    if 'YEAR' in df.columns or 'VALUE'  in df.columns:
+         col_list = df.columns.values.tolist()
+         existing_cols = [col for col in col_list if col in df.columns and col not in ['YEAR', 'VALUE']]
+         pivoted_df = df.pivot(index=existing_cols, columns="YEAR", values="VALUE").reset_index()
+         return pivoted_df
+    else:
+        return df
+
+
 def transform_df_data(df):
 
-    data_col_del = ['Data Source', 'Data Estimated', 'Data Estimation Method']
-
-				
-    # Data dictionary for renaming the columns
-    data_col_rename = {
-        'Datetime (UTC)': 'date_and_time',
-        'Country': 'country',
-        'Zone Name': 'zone_name',
-        'Zone Id': 'zone_id',
-        'Carbon Intensity gCO₂eq/kWh (direct)': 'direct_carbon_intensity',
-        'Carbon Intensity gCO₂eq/kWh (LCA)': 'lca_carbon_intensity',
-        'Low Carbon Percentage': 'low_carbon_percentage',
-        'Renewable Percentage': 'renewable_percentage'
-    }
-
-    # Columns containing date information
-    date_columns = ['date_and_time']
-
+    data_col_del = ['1970', '1971', '1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990', 'LEVEL_ID','TERRITORY_ID','DATE']
+		
      # Dropping unnecessary columns
     df = drop_columns(df, data_col_del)
-
-    # Renaming columns for clarity
-    df = rename_columns(df, data_col_rename)
-
-    # Fixing the date format
-    df = fix_date_format(df, date_columns)
-
-    columns_neg_val = ['direct_carbon_intensity', 'lca_carbon_intensity', 'low_carbon_percentage', 'renewable_percentage']
-    df[columns_neg_val] = df[columns_neg_val].map(lambda x: abs(x) if x < 0 else x)
+    df = transpose_data(df)
 
     return df
 
 def get_source_data(path, delimiter):
-    """
-    Reads data from the mentioned file path into the DataFrame.
+    # Use io.StringIO to simulate a file object
+    response = requests.get(path)
+    csv_file = io.StringIO(response.text)
 
-    Parameters:
-    path (str): The file path from which the data is to be read.
-    delimiter (str): Indicates the delimiter used in the file (e.g., ',', ';', '\t', etc.).
+# Read the CSV file and filter out lines ending with ",,,,,,,,"
+    filtered_lines = [line for line in csv_file if not line.strip().endswith(",,,,,,,")]
 
-    Returns:
-    pd.DataFrame: A DataFrame which contains the imported data.
-    """
-    # Using pandas package to read data from the specified file path and the given delimiter
-    df = pd.read_csv(path, delimiter=delimiter)
+# Join the filtered lines into a single string
+    filtered_csv_data = "".join(filtered_lines)
+
+# Use pandas to read the filtered CSV data into a DataFrame
+    df = pd.read_csv(io.StringIO(filtered_csv_data), delimiter=delimiter)   
+    # Using pandas to read data from the specified file path with the given delimiter
+    # df = pd.read_csv(path, delimiter=delimiter)
     return df
 
 def write_to_target(df, engine, table_name):
-    """
-    Writes a pandas DataFrame to a SQL table.
-
-    Parameters:
-    df (pd.DataFrame): The DataFrame to be written to the SQL table.
-    engine: The SQLAlchemy engine instance used to connect to the database.
-    table_name (str): The name of the target table in the SQL database where the data will be written.
-
-    Note: Ensure that the connection specified by 'engine' is open and valid before calling this function.
-    """
-
+   
     # Writing the DataFrame to the specified SQL table without including the DataFrame's index
     df.to_sql(table_name, engine, if_exists='replace', index=False)
 
 
 def automated_data_pipeline(details):
-    germany_df = get_source_data(details['germany_data']['source'], details['germany_data']['delimiter'])
-    britain_df = get_source_data(details['britain_data']['source'], details['britain_data']['delimiter'])
+    world_df = get_source_data(details['world_data']['source'], details['world_data']['delimiter'])
+    europe_df = get_source_data(details['europe_data']['source'], details['europe_data']['delimiter'])
 
-    germany_df = transform_df_data(germany_df)
-    britain_df = transform_df_data(britain_df)
+    world_df = transform_df_data(world_df)
+    europe_df = transform_df_data(europe_df)
 
     engine = create_engine(f"sqlite:///{details['target_db_path']}\\{details['target_db_name']}.db")
 
-    write_to_target(germany_df, engine, details['germany_data']['target_table'])
-    write_to_target(britain_df, engine, details['britain_data']['target_table'])
+    write_to_target(world_df, engine, details['world_data']['target_table'])
+    write_to_target(europe_df, engine, details['europe_data']['target_table'])
 
 if __name__ == '__main__':
     src_tgt_details = {
-        'germany_data' : {
-            'source' : "https://raw.githubusercontent.com/ShubhangiMore0402/Project1-MADE/main/data/DE_2022_hourly.csv",
+        'world_data' : {
+            'source' : "https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/EDGAR/datasets/v432_FT2016/EDGARv432_FT2016_CO2_per_GDP_emissions_1970-2016.csv",
             'delimiter' : ',',
-            'target_table' : 'germany_data'
+            'target_table' : 'world_data'
         },
-        'britain_data' : {
-            'source' : "https://raw.githubusercontent.com/ShubhangiMore0402/Project1-MADE/main/data/GB_2022_hourly.csv",
+        'europe_data' : {
+            'source' : "https://urban.jrc.ec.europa.eu/api/udp/v2/en/data/?databrick_id=739&nutslevel=0&ts=TOURISM&nutsversion=-1&mpx=1&nutslevel=9&format=csv",
             'delimiter' : ',',
-            'target_table' : 'britain_data'
+            'target_table' : 'europe_data'
         },
-        'target_db_path' : '../data',        
+        'target_db_path' : './data',        
         'target_db_name' : 'made-project_new'
     }
 
-    germany_df = get_source_data(src_tgt_details['germany_data']['source'], src_tgt_details['germany_data']['delimiter'])
-    britain_df = get_source_data(src_tgt_details['britain_data']['source'], src_tgt_details['britain_data']['delimiter'])
+    world_df = get_source_data(src_tgt_details['world_data']['source'], src_tgt_details['world_data']['delimiter'])
+    europe_df = get_source_data(src_tgt_details['europe_data']['source'], src_tgt_details['europe_data']['delimiter'])
     
-    germany_df = transform_df_data(germany_df)
-    britain_df = transform_df_data(britain_df)
+  
+    world_df = transform_df_data(world_df)
+    europe_df = transform_df_data(europe_df)
 
     # Ensure directory exists
     os.makedirs(src_tgt_details['target_db_path'], exist_ok=True)
 
     engine = create_engine(f"sqlite:///{src_tgt_details['target_db_path']}//{src_tgt_details['target_db_name']}.db")
-    write_to_target(germany_df, engine, src_tgt_details['germany_data']['target_table'])
-    write_to_target(britain_df, engine, src_tgt_details['britain_data']['target_table'])
+    write_to_target(world_df, engine, src_tgt_details['world_data']['target_table'])
+    write_to_target(europe_df, engine, src_tgt_details['europe_data']['target_table'])
 
     engine.dispose()
+
